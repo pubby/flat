d5ecdb7eb1735547577b35c16e4947adfaec4c18
pubby <pubby8@gmail.com>
2016-08-15 04:06:58 -0500

    Add license comment.

--- include/impl/class_def.hpp
+++ include/impl/class_def.hpp
@@ -1,3 +1,7 @@
+// Copyright Pubby 2016
+// Distributed under the Boost Software License, Version 1.0.
+// http://www.boost.org/LICENSE_1_0.txt
+
 private:
     using D = FLATNAME;
     using Key = typename Container::value_type;
