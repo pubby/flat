86fb4d3c8e7475ce2c265bff7df0e670d70a52f4
Jason Dictos <jason.dictos@aparavi.com>
2019-09-15 14:15:17 -0700

    Allow for transparent comparator types to get passed through the first_compare wrapper down to the callers own comparator instance

--- include/impl/flat_impl.hpp
+++ include/impl/flat_impl.hpp
@@ -21,6 +21,9 @@ constexpr container_construct_t container_construct = {};
 
 namespace impl {
 
+template<typename Key>
+using transparent_key_t = std::reference_wrapper<Key>;
+
 template<typename Comp>
 struct eq_comp
 {
@@ -117,7 +120,7 @@ public:
     It underlying;
 };
 
-template<typename Pair, typename Compare = std::less<void>>
+template<typename Pair, typename Compare = std::less<void>, typename TransparentKey = void>
 struct first_compare
 {
 	first_compare(const Compare& comp) :
@@ -129,16 +132,38 @@ struct first_compare
         return compare.get()(lhs.first, rhs.first);
     }
 
-    bool operator()(typename Pair::first_type const& lhs,
-                    Pair const& rhs) const
+    bool operator()(typename Pair::first_type const& lhs, Pair const& rhs) const
     {
         return compare.get()(lhs, rhs.first);
     }
 
-    bool operator()(Pair const& lhs,
-                    typename Pair::first_type const& rhs) const
+    bool operator()(Pair const& lhs, typename Pair::first_type const& rhs) const
     {
         return compare.get()(lhs.first, rhs);
+    }
+
+	template<typename Key>
+    bool operator()(transparent_key_t<Key> const& lhs, Pair const& rhs) const
+    {
+        return compare.get()(lhs.get(), rhs.first);
+    }
+
+	template<typename Key>
+    bool operator()(transparent_key_t<Key> const& lhs, typename Pair::first_type const& rhs) const
+    {
+        return compare.get()(lhs.get(), rhs);
+    }
+
+	template<typename Key>
+    bool operator()(Pair const& lhs, transparent_key_t<Key> const& rhs) const
+    {
+        return compare.get()(lhs.first, rhs.get());
+    }
+
+	template<typename Key>
+    bool operator()(typename Pair::first_type const& lhs, transparent_key_t<Key> const& rhs) const
+    {
+		return compare.get()(lhs, rhs.get());
     }
 
     std::reference_wrapper<const Compare> compare;
@@ -375,7 +400,7 @@ public:
     const_iterator find(K const& key) const
     {
         const_iterator it = self()->lower_bound(key);
-        if(it == self()->end() || self()->value_comp()(key, *it))
+		if (it == self()->end() || self()->value_comp()(std::ref(key), *it))
             return self()->end();
         return it;
     }
@@ -384,7 +409,7 @@ public:
     iterator find(K const& key)
     {
         iterator it = self()->lower_bound(key);
-        if(it == self()->end() || self()->value_comp()(key, *it))
+		if (it == self()->end() || self()->value_comp()(std::ref(key), *it))
             return self()->end();
         return it;
     }
@@ -394,7 +419,7 @@ public:
     {
          return std::lower_bound(
              self()->begin(), self()->end(),
-             key, self()->value_comp());
+			 std::ref(key), self()->value_comp());
     }
 
     template<typename K>
@@ -402,7 +427,7 @@ public:
     {
          return std::lower_bound(
              self()->begin(), self()->end(),
-             key, self()->value_comp());
+			 std::ref(key), self()->value_comp());
     }
 
     template<typename K>
@@ -410,7 +435,7 @@ public:
     {
          return std::upper_bound(
              self()->begin(), self()->end(),
-             key, self()->value_comp());
+			 std::ref(key), self()->value_comp());
     }
 
     template<typename K>
@@ -418,7 +443,7 @@ public:
     {
          return std::upper_bound(
              self()->begin(), self()->end(),
-             key, self()->value_comp());
+			 std::ref(key), self()->value_comp());
     }
 
     template<typename K>
@@ -427,7 +452,7 @@ public:
     {
          return std::equal_range<const_iterator>(
              self()->begin(), self()->end(),
-             key, self()->value_comp());
+			 std::ref(key), self()->value_comp());
     }
 
     template<typename K>
@@ -435,7 +460,7 @@ public:
     {
          return std::equal_range<iterator>(
              self()->begin(), self()->end(),
-             key, self()->value_comp());
+			 std::ref(key), self()->value_comp());
     }
 };
 
